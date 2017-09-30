0b5ca8da0904d3f3f5eba2a161e3aa74c08a3056
823903173 <823903173@qq.com>
2017-09-30 18:19:58 +0800

    更新部分测试

--- src/main/java/com/hmcc/contact/web/controller/CollectNewLogController.java
+++ src/main/java/com/hmcc/contact/web/controller/CollectNewLogController.java
@@ -1,9 +1,17 @@
 package com.hmcc.contact.web.controller;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.hmcc.contact.entity.Demo;
+import com.hmcc.contact.entity.NewLog;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.hmcc.contact.service.INewLogService;
+import sun.security.util.BigInt;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by a8239 on 2017/9/29.
@@ -11,12 +19,22 @@ import java.util.ArrayList;
 @RequestMapping("/collectnewlog")
 @Controller
 public class CollectNewLogController extends AbstractController {
+    private INewLogService iNewLogService;
+
     private final Logger logger = Logger.getLogger(CollectNewLogController.class);
 
+    @GetMapping("list")
+    public List<NewLog> list() {
+        EntityWrapper<NewLog> newlogEntityWrapper = new EntityWrapper<>();
+        return iNewLogService.selectList(newlogEntityWrapper);
+    }
+
     @RequestMapping("addnewlog")
-    public void addNewLog(ArrayList<String> list){
-
-
-
+    public void addNewLog(ArrayList<NewLog> list){
+//        EntityWrapper<NewLog> demoEntityWrapper = new EntityWrapper<>();
+        NewLog newLogEntityWrapper = new NewLog();
+        newLogEntityWrapper.setAdmin_id(2);
+        newLogEntityWrapper.setOperations_log("asdasdasd");
+//        return iNewLogService.insert(newLogEntityWrapper);
     }
 }
