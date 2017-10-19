0912c93a1ac5586f48ae71e7772f56d4b780b78e
823903173 <823903173@qq.com>
2017-10-19 20:30:11 +0800

    g展示三级结构  新需求

--- src/main/java/com/hmcc/contact/web/controller/OrganizationController.java
+++ src/main/java/com/hmcc/contact/web/controller/OrganizationController.java
@@ -66,6 +66,10 @@ public class OrganizationController {
     public void getIdByGroupId(HttpServletResponse response, HttpServletRequest request,String id){
         JSONObject json = new JSONObject();
         List<Organization> res = iOrganizationService.getIdByGroupId(id);
+        /*查询组织下所有组织
+        *
+        * 如果是空
+        * 查询该组织ID下所有人员信息列表*/
         if (res.size()==0 || res.isEmpty()){
             System.out.println("res shi kong");
             List<AddresslistUser> resOfUser = iAddresslistUserService.getOnesByDepart(id);
@@ -73,24 +77,56 @@ public class OrganizationController {
             json.put("value",resOfUser);
             DoAjax.doAjax(response, json, null);
         }else {
+//          * 如果是不空
+//          * 返回下级所有组织结构
             System.out.println("RES BUSHI KONG");
             json.put("isEnd",false);
             json.put("value",res);
             DoAjax.doAjax(response, json, null);
         }
-
-
-
-
 //        System.out.println(res);
+    }
 
+
+
+
+
+    /*首页展示
+     * 传入 手机号码
+     * 返回 所在级别的上两层组织结构
+      *eg：18803712866
+      *组织ID：030-017-005
+      * 河南省（31个地区） 许昌市（18地市） 许昌县（五县一区）
+      *
+      *
+      * 手机号码 查 组织ID
+      *
+      * 获取组织ID后判断长度，查询
+      * */
+    @GetMapping("showIndexPage")
+    public void showIndexPage(HttpServletResponse response, HttpServletRequest request,long phoneNumber){
+        JSONObject json = new JSONObject();
+
+        String s = "000";
+        List<AddresslistUser> list =iAddresslistUserService.searchByPhoneNum(phoneNumber);
+
+        s = list.get(0).getGroupId();
+        if (s.substring(0,s.length()-6)!=null){
+            List<AddresslistUser> fatherList = iAddresslistUserService.getOnesByDepart(s.substring(0,s.length()-3));
+            json.put("list",list);
+            json.put("fatherList",fatherList);
+            DoAjax.doAjax(response, json, null);
+        }else {
+            List<AddresslistUser> fatherList = iAddresslistUserService.getOnesByDepart(s.substring(0,s.length()-3));
+            List<AddresslistUser> fatherFatherList = iAddresslistUserService.getOnesByDepart(s.substring(0,s.length()-6));
+            json.put("list",list);
+            json.put("fatherList",fatherList);
+            json.put("fatherFatherList",fatherFatherList);
+            DoAjax.doAjax(response, json, null);
+        }
 
     }
 
-    /*
-    *
-    *
-    * */
 
 
 
