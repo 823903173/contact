d9f6581655f2f26b050b6ca011627adf25e3b897
823903173 <823903173@qq.com>
2017-10-27 14:22:16 +0800

    最后

--- src/main/webapp/platform/addressList/admin/js/login.js
+++ src/main/webapp/platform/addressList/admin/js/login.js
@@ -12,6 +12,7 @@ $(function(){
     $('.login-btn').click(function () {
         if (!validPhone()) {
             return;
+            
         }
         var phone = $('#phone').val();
         var msm = $('#yzm').val();
